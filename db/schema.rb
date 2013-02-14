3fafec178888ff9e1789cc78bd1299c8c1b956e2
Justin D. Thomas <justmas@gmail.com>
2013-02-14 12:56:44 -0500

    added field image_link and edited views to show field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130126191501) do
+ActiveRecord::Schema.define(:version => 20130214173003) do
 
   create_table "recipes", :force => true do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130126191501) do
     t.text     "instructions"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+    t.string   "image_link"
   end
 
 end
